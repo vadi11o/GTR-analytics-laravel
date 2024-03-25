2ed3ec94d7fd839755a16d7cec03f476b725f651
vadillo <vadillo.139009@e.unavarra.es>
2024-03-25 19:40:16 +0100

    [7] - Hechas migraciones bbdd TwitchAnalitics

---
+++ database/migrations/0001_01_01_000003_create_users_table_ta.php
@@ -0,0 +1,31 @@
+<?php
+// create_users_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('users_ta', function (Blueprint $table) {
+            $table->id();
+            $table->string('twitch_id', 255);
+            $table->string('login', 255);
+            $table->string('display_name', 255);
+            $table->string('type', 50);
+            $table->string('broadcaster_type', 50);
+            $table->text('description');
+            $table->string('profile_image_url', 255);
+            $table->string('offline_image_url', 255);
+            $table->integer('view_count');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users_ta');
+    }
+};
